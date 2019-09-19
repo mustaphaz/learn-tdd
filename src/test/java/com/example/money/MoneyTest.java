76035b26985fb76a18ab04583be030e8963c4968
mustapha <mzeamari@bol.com>
2019-09-19 08:31:24 +0200

    initial commit:     - setup gradle     - setup package structure     - create test class with dummy test

---
+++ src/test/java/com/example/money/MoneyTest.java
@@ -0,0 +1,13 @@
+package com.example.money;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoneyTest {
+
+    @Test
+    public void test() {
+        assertThat(1).isEqualTo(1);
+    }
+}
